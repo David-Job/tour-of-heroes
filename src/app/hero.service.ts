bab30a1b200af174d719c793e5a19876bd4b9589
David-Job <17304096+David-Job@users.noreply.github.com>
2021-07-13 11:47:40 +0100

    Error handling inside hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,29 +1,52 @@
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-
   private heroesUrl = 'api/heroes'; // URl to web api
 
-  constructor(public messageService: MessageService, private http : HttpClient) {}
+  constructor(
+    public messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    //const heroes=of(HEROES);
-    const heroes = this.http.get<Hero[]>(this.heroesUrl)
-    this.log('fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
